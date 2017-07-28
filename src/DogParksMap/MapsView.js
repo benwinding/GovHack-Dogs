fe0531b791b72fc20a26a7ed3954f06eaf6e24e4
Ben Winding <ben.winding@gmail.com>
2017-07-29 00:48:48 +0930

    Added map locations repository

--- src/DogParksMap/MapsView.js
+++ src/DogParksMap/MapsView.js
@@ -7,6 +7,15 @@ import MapPoint from "./MapPoint";
 const gmapsApiKey = "AIzaSyAFVS3VoZHTceJd3snrMVWb1NtihK8XsVk";
 
 class MapsView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {points: [<MapPoint
+      lat={-36}
+      lng={138}
+      text={"2"}
+    />]};
+  }
+
   render() {
     return (
       <div style={{width:"100vw", height:"100vh"}} >
@@ -15,21 +24,25 @@ class MapsView extends Component {
           center={this.props.center}
           zoom={this.props.zoom}
         >
-          { MapsView.GetPoints() }
+          {this.state.points}
         </GoogleMap>
       </div>
     );
   }
 
-  static GetPoints() {
-    let points = PointsRepository.GetPointsExample();
-    return points.map((point) =>
-      <MapPoint
-        lat={point.lat}
-        lng={point.lng}
-        text={point.txt}
-      />
-    );
+  componentDidMount() {
+    PointsRepository.GetPointsFromApi("")
+    .then((points) => {
+      console.log(points);
+      let pointComponents = points.map((point) => {
+        return <MapPoint
+          lat={point.lat}
+          lng={point.lng}
+          text={point.name}
+        />;
+      });
+      this.setState({points: pointComponents})})
+    .catch((val) => console.log(val));
   }
 }
 
--- src/DogParksMap/PointsRepository.js
+++ src/DogParksMap/PointsRepository.js
@@ -1,10 +1,20 @@
 import * as React from "react";
+const rp = require('request-promise-native');
 
 export default class PointsRepository {
-  static GetPointsFromApi() {
+  static GetPointsFromApi(suffix) {
+    let url = "http://gov-hack-test-api.glitch.me/"+suffix;
+    return new Promise((resolve, reject) => {
+      rp({uri: url})
+        .then((body) => {
+          resolve(JSON.parse(body))
+        })
+        .catch(reject);
+    });
   }
 
+
   static GetPointsExample() {
-    return [{lat:-34.6,lng:138.8,txt:"A"},{lat:-34.3,lng:138.8,txt:"B"}];
+    return [{lat:-34.6,lng:138.8,name:"A"},{lat:-34.3,lng:138.8,name:"B"}];
   }
 }
