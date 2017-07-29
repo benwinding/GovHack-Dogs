e28152ddb27272a505c2460dc3a94b5b689c7082
Ren Koh <ren@Rens-MacBook-Pro-2712.local>
2017-07-29 17:30:33 +0930

    add thing

--- flask-api/backend.py
+++ flask-api/backend.py
@@ -8,14 +8,37 @@ e = create_engine('sqlite:///parks.db')
 app = Flask(__name__)
 api = Api(app)
 
+
 class Parks(Resource):
     def get(self):
+        lat = request.args.get('lat')
+        lng = request.args.get('lng')
         conn = e.connect()
-        query = conn.execute("SELECT ParkName, ParkLocation, Lat, Long FROM parks")
+        query = conn.execute("SELECT _rowid_, ParkName, lat, lng \
+                              FROM parksdata \
+                              WHERE activities__Dogwalking = 'TRUE'")
+        data = []
+        for c in query.cursor:
+            distance = (float(c[2]) - float(lat))**2 + (float(c[3]) - float(lng))**2
+            data.append({'parkid': c[0], 'ParkName':c[1], 'lat':c[2], 'lng':c[3], 'distance':distance})
+
+        data.sort(key=lambda x: x['distance'])
+
+        return {'data':data[:10]}, {'Content-Type': 'application/json', 'Access-Control-Allow-Origin':'*'}
+
+class Park(Resource):
+    def get(self):
+        id = request.args.get('parkid')
+        conn = e.connect()
+        query = conn.execute("SELECT ParkName, lat, lng, facilities__Toilet, 'facilities__Toilet(accessible)', facilities__Babychangeroom, facilities__Shower, facilities__Parking, facilities__Boatramp, facilities__Shelter, facilities__Picnictable, facilities__Playground, facilities__Sportsfield, facilities__Cafe, facilities__Publictransportaccess, facilities__BBQfacilities\
+                              FROM parksdata\
+                              WHERE _rowid_ =" + id)
         return {'data': [dict(zip(tuple (query.keys()), i)) for i in query.cursor]}, {'Content-Type': 'application/json', 'Access-Control-Allow-Origin':'*'}
 
 
 api.add_resource(Parks, '/parks')
+api.add_resource(Park, '/park')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
