0155ed01604562d001561521177877653d84481c
Ben Winding <ben.winding@gmail.com>
2017-07-30 16:53:08 +0930

    Beta stamp added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import MyMenu from "./MyMenu/MyMenu";
 import MyMenuRoutes from "./MyMenu/MyMenuRoutes";
-import MenuFooter from "./FooterMenu/MenuFooter";
+import betaTag from './beta-stamp.png';
 
 export default class App extends Component {
   render() {
     return (
       <div className="App" id="outer-container">
+        <img src={betaTag} className="betaTag"/>
         <MyMenu />
         <MyMenuRoutes id="page-wrap"/>
       </div>
