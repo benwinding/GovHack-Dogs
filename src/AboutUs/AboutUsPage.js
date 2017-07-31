954a7bfc0eed38909956fd3377a73666b0716757
Ben <ben.winding@gmail.com>
2017-07-31 18:33:49 +0930

    Fixed about page styling

--- src/AboutUs/AboutUsPage.js
+++ src/AboutUs/AboutUsPage.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {ControlLabel, FormControl, FormGroup, Radio, Button, Row, Col,} from 'react-bootstrap';
+import {Row, Col, Grid,} from 'react-bootstrap';
 import './AboutUsStyle.css';
 import photo from '../photo.jpg';
 import logo from '../logo.png';
@@ -9,43 +9,46 @@ export default class AboutUsPage extends Component {
   render() {
     return (
       <div className="about">
-        <Row>
-          <Col sm="4">
-          </Col>
-          <Col sm="4">
-            <p className="title"> About Us! </p>
-          </Col>
-          <Col sm="4">
-          </Col>
-        </Row>
-        <Row>
-          <Col sm="6">
-            <img src={ photo } alt="Our photo" responsive height="400" width="100%"/>
-          </Col>
-          <Col sm="5">
-            <p >
-              We are all Post Graduate Computer Science students at the University of Adelaide who have come together for the GovHack event, we all share a desire to make the community better and a love for dogs.
-            </p>
-          </Col>
-        </Row>
-
-        <Row>
-          <Row sm="6">
-            <img src={ logo } alt="Logo" responsive height="100" width="150"/>
+        <Grid>
+          <Row>
+            <Col sm="12">
+              <div className="title">About Us!</div>
+            </Col>
           </Row>
-          <Row sm="6">
-            <h3>Dog parks of Adelaide</h3>
-            <p >
-              As a dog owner I never can tell what areas are dog friendly and when I'm at a dog friendly area I never know the particular details regarding that area.  Our project solves that problem.  We have collated data from Department of Environment Water and Natural Resources regarding hiking and wilderness facilities with data from local councils regarding dog park locations.  The result is a one stop comprehensive database of dog friendly areas in SA.
-              Our App uses location services to easily filter the relevant information and provide dog owners with all the details that they want with one click.
-            </p>
-            <div>
-              <a href="https://youtu.be/GB8HMi2tBps">
-                Pitch Video: https://youtu.be/GB8HMi2tBps
-              </a>
-            </div>
+          <Row>
+            <Col sm="6">
+              <img src={ photo } alt="Our photo" className="aboutImage"/>
+            </Col>
+            <Col sm="6">
+              <p>
+                We are all Post Graduate Computer Science students at the University of Adelaide who have come
+                together for the GovHack event, we all share a desire to make the community better and a love for dogs.
+              </p>
+            </Col>
+            <Col sm="6">
+              <img src={ logo } alt="Logo" className="aboutLogo"/>
+            </Col>
+            <Col sm="6">
+              <h3>Dog parks of Adelaide</h3>
+              <p>
+                As a dog owner I never can tell what areas are dog friendly and when I'm at a dog friendly
+                area I never know the particular details regarding that area.  Our project solves that problem.
+                We have collated data from Department of Environment Water and Natural Resources regarding hiking
+                and wilderness facilities with data from local councils regarding dog park locations.  The result
+                is a one stop comprehensive database of dog friendly areas in SA.
+              </p>
+              <p>
+                Our App uses location services to easily filter the relevant information and provide dog owners
+                with all the details that they want with one click.
+              </p>
+              <div className="videoLink">
+                <a href="https://youtu.be/GB8HMi2tBps">
+                  Pitch Video: https://youtu.be/GB8HMi2tBps
+                </a>
+              </div>
+            </Col>
           </Row>
-        </Row>
+        </Grid>
       </div>
     )
   }
